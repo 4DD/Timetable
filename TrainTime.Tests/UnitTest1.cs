c8af1f38a2ea689d961cc2befd8694d8305ade55
Dinesh <dineshpdn@hotmail.com>
2018-07-05 08:25:49 +1000

    test projects

---
+++ TrainTime.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrainTime.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
